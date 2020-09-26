4db357a801f2e5bb4baef696c572be8891e38676
DipCoy <dipolarcoyote@gmail.com>
2020-09-26 17:49:55 +0500

    Add Speed Up

--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -2,31 +2,26 @@
 #include "button.h"
 #include "buzzer.h"
 
-#define PIN_BUTTON_MEL_ONE 3
-#define PIN_BUTTON_OFF 5
-#define PIN_BUTTON_SPEED 4
+#define PIN_BUTTON_MEL_ONE 4
+#define PIN_BUTTON_MEL_TWO 5
+#define PIN_BUTTON_SPEED_UP 3
+#define PIN_BUTTON_SPEED_DOWN 2
 #define PIN_BUZZER 6
 
+const int DELTA_SPEED = 15;
+const int MIN_SPEED = 2000;
+
 Button buttonMelodyOne(PIN_BUTTON_MEL_ONE);
-Button buttonOff(PIN_BUTTON_OFF);
-Button buttonSpeed(PIN_BUTTON_SPEED);
+Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
+Button buttonSpeedUp(PIN_BUTTON_SPEED_UP);
+Button buttonSpeedDown(PIN_BUTTON_SPEED_DOWN);
 Buzzer buzzer(PIN_BUZZER);
 
 int notes[] = {NOTE_A4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
 double durations[] = {8, 1, 4, 1};
 int melodyLength = 4;
 
-unsigned long speeds[] = {25, 50, 100, 200, 400, 800};
-int currentSpeed = 2;
-int speedsLength = 6;
-
-// maybe somewhere in the future we will have one more button...
-#define PIN_BUTTON_MEL_TWO 4
-Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
-//and the second melody
-int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
-double durations2[] = {4, 1, 4, 1};
-int melodyLength2 = 4;
+int currentSpeed = 100;
 
 int notes3[] = {NOTE_D4, NOTE_A4, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_A4, NOTE_FS5, NOTE_A4, 
 NOTE_D4, NOTE_A4, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_A4, NOTE_FS5, NOTE_A4,
@@ -48,20 +43,21 @@ void loop()
 {
     buzzer.playSound();
 
-    if (buttonOff.wasPressed())
-    {
-        buzzer.turnSoundOff();
-    }
-
     if (buttonMelodyOne.wasPressed())
     {
         buzzer.setMelody(notes, durations, melodyLength);
         buzzer.turnSoundOn();
     }
 
-    if (buttonSpeed.wasPressed())
+    if (buttonMelodyTwo.wasPressed())
     {
-        currentSpeed = (currentSpeed + 1)%speedsLength;
-        buzzer.setNoteDuration(speeds[currentSpeed]);
+        buzzer.setMelody(notes3, durations3, melodyLength3);
+        buzzer.turnSoundOn();
+    }
+
+    if (buttonSpeedUp.wasPressed())
+    {
+        currentSpeed = max(currentSpeed - DELTA_SPEED, 1);
+        buzzer.setNoteDuration(currentSpeed);
     }
 }
